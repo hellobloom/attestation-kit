8a90d713cbc95bd12f23bf4595e27853a9d26bf7
djvs <djvansch@gmail.com>
2018-11-01 13:17:54 -0400

    fix

--- app/migrations/20181028154013-create-ping.js
+++ app/migrations/20181028154013-create-ping.js
@@ -3,8 +3,11 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.sequelize.query(`
       alter type enum_whisper_msg_types add value 'ping';
+    `)
+    await queryInterface.sequelize.query(`
       alter type enum_whisper_msg_types add value 'pong';
-
+    `)
+    await queryInterface.sequelize.query(`
       create table "whisper_pings" (
         "id" uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
         "created" timestamp without time zone default now() not null,
